a4e1979871f6d5184f980870215b49dbd0461266
Phillipe Lopes <phillipe.alcantara.fonseca@gmail.com>
2016-09-27 10:44:59 -0300

    callback updateLinhas criado

--- src/home.js
+++ src/home.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {View, StyleSheet} from 'react-native';
+import Fuse from 'fuse.js';
 
 import linhas from '../linhas.json';
 
@@ -10,13 +11,38 @@ export default class Home extends Component {
 
   constructor() {
     super();
+
+    this.state = {
+      linhas
+    };
+
+    this.updateLinhas.bind(this);
+  }
+
+  updateLinhas(query) {
+    const options = {
+      shouldSort: true,
+      threshold: 0.6,
+      location: 0,
+      distance: 100,
+      maxPatternLength: 32,
+      keys: [
+        "title",
+        "author.firstName"
+      ]
+    };
+
+    const fuse = new Fuse(this.state.linhas, options);
+    const result = fuse.search(query);
+
+    this.setState({linhas: result});
   }
 
   render() {
     return (
       <View style={styles.container} refreshing>
         <SearchBox />
-        <LinhasList linhas={linhas}/>
+        <LinhasList linhas={this.state.linhas} updateLinhas={this.updateLinhas} />
       </View>
     );
   }
