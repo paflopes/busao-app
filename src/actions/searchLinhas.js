df20eb34b2228b42895a6fb6809266406cb4dbdd
Phillipe Lopes <phillipe.alcantara.fonseca@gmail.com>
2016-10-18 19:23:42 -0200

    search logic moved to reducer

--- src/actions/searchLinhas.js
+++ src/actions/searchLinhas.js
@@ -1,36 +1,6 @@
-import Fuse from 'fuse.js';
-
-import linhas from './../../linhas.json';
-
-let fuse = null;
-
-const options = {
-  shouldSort: true,
-  threshold: 0.6,
-  location: 0,
-  distance: 100,
-  maxPatternLength: 32,
-  keys: [
-    {name: "numero", weight: 0.4},
-    {name: "nome", weight: 0.4},
-    {name: "caminho.ida", weight: 0.1},
-    {name: "caminho.volta", weight: 0.1},
-  ]
-};
-
 export const searchLinhas = (query = null) => {
-  if (!fuse) {
-    fuse = new Fuse(linhas, options);
-  }
-
-  let payload = linhas;
-
-  if (query && query !== '') {
-    payload = fuse.search(query);
-  }
-
   return {
     type: 'SEARCH_LINHAS',
-    payload
+    payload: query
   };
 };
--- src/reducers/reducerLinhas.js
+++ src/reducers/reducerLinhas.js
@@ -1,9 +1,41 @@
+import Fuse from 'fuse.js';
+
 import linhas from './../../linhas.json';
 
 export default (state, action) => {
   switch (action.type) {
     case 'SEARCH_LINHAS':
-      return action.payload;
+      return searchLinhas(action.payload);
   }
   return linhas;
+};
+
+let fuse = null;
+
+const options = {
+  shouldSort: true,
+  threshold: 0.6,
+  location: 0,
+  distance: 100,
+  maxPatternLength: 32,
+  keys: [
+    {name: "numero", weight: 0.4},
+    {name: "nome", weight: 0.4},
+    {name: "caminho.ida", weight: 0.1},
+    {name: "caminho.volta", weight: 0.1},
+  ]
+};
+
+const searchLinhas = (query = null) => {
+  if (!fuse) {
+    fuse = new Fuse(linhas, options);
+  }
+
+  let result = linhas;
+
+  if (query && query !== '') {
+    result = fuse.search(query);
+  }
+
+  return result;
 };
